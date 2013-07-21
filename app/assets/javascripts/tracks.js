198ff192f1ab81a11eaead86409b9ad7c5381f55
odesey <odesey@gmail.com>
2013-07-21 11:31:40 -0400

    started work on the track index jquery tables feature

--- app/assets/javascripts/tracks.js
+++ app/assets/javascripts/tracks.js
@@ -1,5 +1,25 @@
 $(function(){
 
+    $('.stopbutton').hide();
+    $('.playbutton').click(function() {
+        
+        soundManager.play('mySound-'+$(this).data("track-id"));
+        var play_id = '.playbutton[data-track-id="'+$(this).data("track-id")+'"]';
+        var stop_id = '.stopbutton[data-track-id="'+$(this).data("track-id")+'"]';
+        $(play_id).hide();
+        
+        $(stop_id).show();
+    });
+
+    $('.stopbutton').click(function() {
+      soundManager.pause('mySound-'+$(this).data("track-id"));
+      var play_id = '.playbutton[data-track-id="'+$(this).data("track-id")+'"]';
+      var stop_id = '.stopbutton[data-track-id="'+$(this).data("track-id")+'"]';
+      $(stop_id).hide();
+      $(play_id).show();
+
+    });
+
     //Backgrid part
 
     var Territory = Backbone.Model.extend({});
